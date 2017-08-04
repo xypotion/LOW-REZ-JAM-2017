6e29c05fe1f0cc40d874a75fe49da530988f2394
Max W <xypotion@gmail.com>
2017-08-04 06:45:40 -0700

    being slightly dangerous by not fully testing this fix... but actuations should no longer go below 0 or over maximum

--- queueProcessing.lua
+++ queueProcessing.lua
@@ -58,17 +58,17 @@ end
 --TODO regardless of delta, shown should never go over max or under 0; finish if you hit those
 --TODO pos/neg sounds
 function processActuationEvent(e)
-	--decrement shown and increment delta OR vice-versa
-	if e.delta > 0 then
+	--decrement shown and increment delta OR vice-versa, as long as shown is not already at max or 0
+	if e.delta > 0 and e.counter.shown < e.counter.max then
 		e.counter.shown = e.counter.shown + 1
 		e.delta = e.delta - 1
-	elseif e.delta < 0 then
+	elseif e.delta < 0 and e.counter.shown > 0 then
 		e.counter.shown = e.counter.shown - 1
 		e.delta = e.delta + 1
 	end
 	
-	--finished if delta is depleted
-	if e.delta == 0 then
+	--finished if delta is depleted OR shown is at max or shown is at 0
+	if e.delta == 0 or e.counter.shown >= e.counter.max or e.counter.shown <= 0 then
 		e.finished = true
 	end
 end
