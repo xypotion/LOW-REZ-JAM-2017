35ba056b410e5fba65d703ac6b476abdf03f25af
Max W <xypotion@gmail.com>
2017-08-04 19:26:48 -0700

    kinda working melee AI, except big bug: they still see hero's CURRENT location, not planned location. reserved/vacating won't cut it here, sadly. will have to undo that stuff and just queue enemy turns differently

--- enemyAI.lua
+++ enemyAI.lua
@@ -1,20 +1,32 @@
 function queueEnemyTurn()
-	--DEBUG
+	--fade to night
 	queue(bgEvent("night1", 0.5))
-	-- hero.sp.actual = hero.sp.actual - 1
-	-- queue(actuationEvent(hero.sp, -1))
-	-- queue(bgEvent("night1", 1)) --fake "wait"
-	-- local xxx, yyy = math.random(3), math.random(3) --spawny
-	-- if stage.field[yyy][xxx].class == "clear" then
-	-- 	queue(cellOpEvent(yyy, xxx, enemy("toxy")))
-	-- end
+	
+	--all enemies take their turns
+	local cells = shuffledCellList()
+	for i, coords in ipairs(cells) do
+		local c = stage.field[coords[1]][coords[2]]
+		--TODO what if enemy has >1 AP? gotta loop again here (maybe just a "while")
+		if c and c.contents and c.contents.class and c.contents.class == "enemy" then
+			if c.contents.ai == "melee" then
+				meleeTurnAt(coords[1], coords[2])
+			elseif c.contents.ai == "ranged" then
+				rangedTurn(c.contents)
+			elseif c.contents.ai == "healer" then
+				healerTurn(c.contents)
+			end
+		end
+	end
+	
+	--pull from stage.enemylist to spawn new enemies
 	spawnEnemies()
-	hero.ap.actual = 3 --ap reset
+	
+	--reset hero AP & fade back to day
+	hero.ap.actual = hero.ap.max --ap reset
 	queueSet({
 		bgEvent("day1", 0.5),
 		actuationEvent(hero.ap, 3)
 	})
-	--END DEBUG
 end
 
 --[[
@@ -25,13 +37,73 @@ for each enemy:
 	before moving, must check destination cell to see if it's already reserved.
 ]]
 
-function meleeTurn()
+function meleeTurnAt(ey, ex)
+	--get adjacent cells
+	local ac = getAdjacentCells(ey, ex)
+	
+	--if hero is adjacent, attack (reducing AP)
+	local heroAdjacent = false
+	local hy, hx = locateHero()
+	for i, c in pairs(ac) do
+		if c.y == hy and c.x == hx then
+			heroAdjacent = true
+		end
+	end
+	
+	if heroAdjacent then
+		enemyAttackHero(ey, ex, hy, hx)
+		return
+	end
+	
+	--otherwise, where's hero? move closer (vertically first). check reservation/vacancy status, too
+	print(ey, ex, "wants to move closer")
 end
 
 function rangerTurn()
 end
 
 function healerTurn()
+end
+
+function enemyAttackHero(ey, ex, hy, hx)
+	local dy, dx = hy - ey, hx - ex
+	local attacker = stage.field[ey][ex].contents
+	
+	--first of all, reduce attacker AP
+	attacker.ap.actual = attacker.ap.actual - 1
+	print("attacker's AP:", stage.field[ey][ex].contents.ap.actual)
+	
+	--reduce hero HP
+	hero.hp.actual = hero.hp.actual - attacker.attack
+
+	--queue enemy attack animation & hero damage actuation
+	queueSet({
+		poseEvent(ey, ex, { --TODO refactor to share pose code with heroFight(), also TODO maybe not idle pose? eh
+			{pose = "idle", yOffset = dy * 4, xOffset = dx * 4},
+			{pose = "idle", yOffset = dy * 5, xOffset = dx * 5},
+			{pose = "idle", yOffset = dy * 2, xOffset = dx * 2},
+			{pose = "idle", yOffset = dy * 1, xOffset = dx * 1},
+			{pose = "idle", yOffset = 0, xOffset = 0},
+		}),
+		actuationEvent(hero.hp, -attacker.attack)
+	})
+	
+	--hero defeated? TODO game over implementation
+end
+
+function enemyMove()
+end
+
+--this feels messy, but might be the best way? hm
+function getAdjacentCells(y, x)
+	local cells = {}
+	
+	if y + 1 <= 3 then push(cells, {y = y + 1, x = x}) end
+	if y - 1 >= 1 then push(cells, {y = y - 1, x = x}) end
+	if x + 1 <= 3 then push(cells, {y = y, x = x + 1}) end
+	if x - 1 >= 1 then push(cells, {y = y, x = x - 1}) end
+	
+	return cells
 end
 
 --might as well put spawning here, too?
@@ -85,4 +157,19 @@ function enemy(species)
 			attack = 1
 		}
 	end
+end
+
+--TODO you know there's a nicer way to do this, lazy bones
+function shuffledCellList()
+	return shuffle({
+		{1, 1},
+		{1, 2},
+		{1, 3},
+		{2, 1},
+		{2, 2},
+		{2, 3},
+		{3, 1},
+		{3, 2},
+		{3, 3},		
+	})
 end
